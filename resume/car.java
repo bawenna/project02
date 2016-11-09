2dbe743481686a5f5009fc8f389d3e6720eb3f72
Wenzhao Zhang <wzhang87@hawk.iit.edu>
2016-11-09 09:28:24 -0800

    reset

---
+++ resume/car.java
@@ -0,0 +1,17 @@
+import java.util.Scanner
+public class car{
+  public static void main(String args[])
+  {
+    car newcar = car();
+    newcar.AddInfo();
+}
+
+public void AddInfo(){
+  Scanner scan = new Scanner(System.in);
+  String carName = scan.next();
+  while(!scanhasNextDouble())
+  {
+    String garbage = scan.nextLine();
+  }
+  dNum = scan.nextDouble();
+}
