ddb67b5ebb1097c48967d1e975016acdb82c0fcf
Wenzhao Zhang <wzhang87@hawk.iit.edu>
2016-11-30 10:30:52 -0800

    tested for js

--- site.js
+++ site.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
     $.get(
       'https://www.zipcodeapi.com/rest/js-rTyMicsfWrUvifDkyeNHgt1EysudYy0pszcKLFZH6Udp9QJI5wqlZs0yUuMS3Niw/info.json/'+loc+'/degrees',
       function (data) {
-        $('#print').append(
         lat = Math.round(data.lat);
         lng = Math.round(data.lng);
-          document.createTextNode("Lat/Long" + lat + "," + lng);
+        $('#print').append(
+        document.createTextNode("Lat/Long" + lat + "," + lng);
         );
       });
       event.preventDefault();
