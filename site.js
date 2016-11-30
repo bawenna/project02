a0d2fc60e56edd3e9eb95abe3b39eaf95c5726f7
Wenzhao Zhang <wzhang87@hawk.iit.edu>
2016-11-30 10:15:00 -0800

    tested for js

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       'https://www.zipcodeapi.com/rest/js-rTyMicsfWrUvifDkyeNHgt1EysudYy0pszcKLFZH6Udp9QJI5wqlZs0yUuMS3Niw/info.json/'+loc+'/degrees',
       function (data) {
         $('#print').append(
-          <p><a href ="'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=find+auto+shop+around+you&rflfq=1&rlha=0&rllag='+data.lat+','+data.lng+'&tbm=lcl&tbs=lf:1,lf_ui:2,lf_pqs:EAE'"></a></p>
+          '<p><a href ="'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=find+auto+shop+around+you&rflfq=1&rlha=0&rllag='+data.lat+','+data.lng+'&tbm=lcl&tbs=lf:1,lf_ui:2,lf_pqs:EAE'"></a></p>'
         );
       });
       event.preventDefault();
