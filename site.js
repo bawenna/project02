6769c0b02b1eb05bd10ab5eb39a971e780b4919a
Wenzhao Zhang <wzhang87@hawk.iit.edu>
2016-11-30 10:48:48 -0800

    tested for js

--- site.js
+++ site.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         lng = Math.round(data.lng);
         $('#print').append(
         document.createTextNode(
-"https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=find+auto+shop+around+you&rflfq=1&rlha=0&rllag=+" lat + ","+lng +"&tbm=lcl&tbs=lf:1,lf_ui:2,lf_pqs:EAE")
+"https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=find+auto+shop+around+you&rflfq=1&rlha=0&rllag=+" + lat + "," +lng +"&tbm=lcl&tbs=lf:1,lf_ui:2,lf_pqs:EAE")
         );
       });
       event.preventDefault();
