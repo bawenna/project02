62a035464a5e773c477b3ad84e7d96dd0bb9fe5c
Wenzhao Zhang <wzhang87@hawk.iit.edu>
2016-11-30 10:06:55 -0800

    tested for js

--- site.js
+++ site.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#w-form').on('submit', function(event) {
     var loc = $('#location').val();
     $.get(
-      'https://www.zipcodeapi.com/rest/gL0WYM5K5cwQDKCWQudvv6qeWwqV2kVi6Wf5JlPOxlEb5g8WKmrgtRupoVkqggJm/info.json/'+loc+'/degrees',
+      'https://www.zipcodeapi.com/rest/js-rTyMicsfWrUvifDkyeNHgt1EysudYy0pszcKLFZH6Udp9QJI5wqlZs0yUuMS3Niw/info.json/'+loc+'/degrees',
       function (data) {
         $('#print').append(
           document.createTextNode("Lat/Long: " + data.lat  + ", " + data.lng)
